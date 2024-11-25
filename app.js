08afbed124e4ace924612bcefbf2d573f243f7e3
Gabriel Bonanato <gabriellopes@suzano.com.br>
2024-11-25 10:57:22 -0300

    changes max random value to 42

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 40;
+let numeroLimite = 42;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 2;
 
@@ -11,7 +11,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 25');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 42');
 }
 
 exibirMensagemInicial();
